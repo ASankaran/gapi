84787e81c61f42fe2a73ab9ca4e6f7728321c543
Arnav Sankaran <arnavsankaran@gmail.com>
2018-03-18 01:28:37 -0500

    Removed negroni dependence

--- main.go
+++ main.go
@@ -7,17 +7,13 @@ import (
     "./database"
     "./middleware"
     "github.com/gorilla/mux"
-    "github.com/urfave/negroni"
 )
 
 func main() {
 	database.Migrate()
     router := mux.NewRouter()
     controllers.SetupRouter(router)
-
-    routerMiddlewre := negroni.Classic()
-    routerMiddlewre.Use(negroni.HandlerFunc(middleware.ContentTypeMiddleware))
-    routerMiddlewre.UseHandler(router)
     
-    log.Fatal(http.ListenAndServe(":8000", routerMiddlewre))
+    router.Use(middleware.ContentTypeMiddleware)
+    log.Fatal(http.ListenAndServe(":8000", router))
 }
