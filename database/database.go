e3088dfa1666ef9504410a3d5ad6ef787d707ed5
Arnav Sankaran <arnavsankaran@gmail.com>
2018-03-24 17:52:34 -0500

    Convert api to aws lambda handler

--- database/database.go
+++ database/database.go
@@ -4,9 +4,20 @@ import (
     "github.com/jinzhu/gorm"
     _ "github.com/go-sql-driver/mysql"
     "../models"
+    "fmt"
 )
 
-var DB, _ = gorm.Open("mysql", "root:pass123@/gapi?charset=utf8&parseTime=True&loc=Local")
+var DB *gorm.DB
+
+func init() {
+	var db, err = gorm.Open("mysql", "root:pass123@tcp(172.17.0.1:3306)/gapi?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	DB = db;
+}
 
 func Migrate() {
 	DB.AutoMigrate(&models.User{})
--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
-    "log"
-    "net/http"
     "./controllers"
     "./database"
     "./middleware"
     "github.com/gorilla/mux"
+
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
 )
 
+var initialized = false
+var gorillaMuxAdapter *gorillamux.GorillaMuxAdapter
+
+func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+    if !initialized {
+        database.Migrate()
+        router := mux.NewRouter()
+        controllers.SetupRouter(router)
+        
+        router.Use(middleware.ErrorMiddleware)
+        router.Use(middleware.ContentTypeMiddleware)
+
+        gorillaMuxAdapter = gorillamux.New(router)
+        initialized = true
+    }
+
+    return gorillaMuxAdapter.Proxy(req)
+}
+
 func main() {
-	database.Migrate()
-    router := mux.NewRouter()
-    controllers.SetupRouter(router)
-    
-    router.Use(middleware.ErrorMiddleware)
-    router.Use(middleware.ContentTypeMiddleware)
-    log.Fatal(http.ListenAndServe(":8000", router))
+    lambda.Start(Handler)
 }
